e1e39471a949f28507fb4f182caf60553d965a9b
Evan Fadanelli <Evan Fadanelli@10.80.126.254>
2018-11-05 11:45:43 -0500

    Evan - Added all classes needed for the lab

---
+++ src/edu/wmich/cs1120/efadanelli/LA5/IFormatExceptionHandler.java
@@ -0,0 +1,5 @@
+package edu.wmich.cs1120.efadanelli.LA5;
+
+public interface IFormatExceptionHandler {
+
+}
---
+++ src/edu/wmich/cs1120/efadanelli/LA5/PhoneNumberFormatException.java
@@ -0,0 +1,5 @@
+package edu.wmich.cs1120.efadanelli.LA5;
+
+public class PhoneNumberFormatException extends Exception {
+
+}
